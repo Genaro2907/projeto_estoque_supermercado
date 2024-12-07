a22491ce1967df72626ff1274be7aef905e663a6
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-12-07 18:18:23 -0300

    Projeto Criado

---
+++ backend/supermercado_estoque/src/main/java/gabrieldev/com/supermercado_estoque/SupermercadoEstoqueApplication.java
@@ -0,0 +1,13 @@
+package gabrieldev.com.supermercado_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadoEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercadoEstoqueApplication.class, args);
+	}
+
+}
---
+++ backend/supermercado_estoque/src/test/java/gabrieldev/com/supermercado_estoque/SupermercadoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrieldev.com.supermercado_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
