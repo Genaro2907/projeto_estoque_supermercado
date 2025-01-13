0d4eb5d468f3b6a8c203a2572d9803b30e2f8594
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-01-13 18:04:24 -0300

    customizing swagger documentation of products and department

--- backend/supermercado_estoque/src/main/java/gabrieldev/com/supermercado_estoque/controllers/DepartmentController.java
+++ backend/supermercado_estoque/src/main/java/gabrieldev/com/supermercado_estoque/controllers/DepartmentController.java
@@ -18,9 +18,16 @@ import gabrieldev.com.supermercado_estoque.model.DTO.DepartmentDTO;
 import gabrieldev.com.supermercado_estoque.model.DTO.SimpleDepartmentDTO;
 import gabrieldev.com.supermercado_estoque.services.DepartmentService;
 import gabrieldev.com.supermercado_estoque.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/department")
+@Tag(name = "Department", description = "Endpoints for Managing department")
 public class DepartmentController {
 
     @Autowired
@@ -29,6 +36,15 @@ public class DepartmentController {
     @GetMapping(value = "/{id}", 
     		produces = {
     				MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Find a Department", description = "Find a Department", tags = {"Department"}, 
+    responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    					@Content(schema = @Schema(implementation = DepartmentDTO.class))}),
+    		@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<DepartmentDTO> findById(@PathVariable Long id) {
         DepartmentDTO departmentDTO = service.findById(id);
         return ResponseEntity.ok(departmentDTO);
@@ -36,6 +52,16 @@ public class DepartmentController {
 
     @GetMapping(produces = {
     		MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Finds all Departments", description = "Finds all Departments", tags = {"Department"}, 
+    responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    					@Content(
+    							mediaType = "application/json",
+    							array = @ArraySchema(schema = @Schema(implementation = DepartmentDTO.class)))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<List<DepartmentDTO>> findAll() {
         List<DepartmentDTO> departments = service.findAll();
         return ResponseEntity.ok(departments);
@@ -45,6 +71,13 @@ public class DepartmentController {
     		MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
     			produces = {
     					MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Adds new Department", description = "Adds new Department", tags = {"Department"}, 
+    responses = {
+    		@ApiResponse(description = "Created", responseCode = "200", content = {
+    					@Content(schema = @Schema(implementation = DepartmentDTO.class))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<DepartmentDTO> create(@RequestBody DepartmentDTO departmentDTO) {
         DepartmentDTO createdDepartment = service.create(departmentDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdDepartment);
@@ -54,12 +87,27 @@ public class DepartmentController {
     		MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
     			produces = {
     					MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Updates a Department", description = "Updates a Department", tags = {"Department"}, 
+    responses = {
+    		@ApiResponse(description = "Updated", responseCode = "200", content = {
+    					@Content(schema = @Schema(implementation = DepartmentDTO.class))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<DepartmentDTO> update(@PathVariable Long id, @RequestBody DepartmentDTO departmentDTO) {
         DepartmentDTO updatedDepartment = service.update(id, departmentDTO);
         return ResponseEntity.ok(updatedDepartment);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes a Department", description = "Deletes a Department", tags = {"Department"}, 
+    responses = {
+    		@ApiResponse(description = "No content", responseCode = "204", content = {@Content}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
@@ -67,6 +115,16 @@ public class DepartmentController {
     @GetMapping(value = "/sector", 
     			produces = {
     					MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Finds all Departments Without Products", description = "Finds all Departments Without Products", tags = {"Department"}, 
+    responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    					@Content(
+    							mediaType = "application/json",
+    							array = @ArraySchema(schema = @Schema(implementation = DepartmentDTO.class)))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<List<SimpleDepartmentDTO>> findDepartmentsWithoutProducts() {
         List<SimpleDepartmentDTO>departments = service.findDepartmentsWithoutProducts();
         return ResponseEntity.ok(departments);
--- backend/supermercado_estoque/src/main/java/gabrieldev/com/supermercado_estoque/controllers/ProductController.java
+++ backend/supermercado_estoque/src/main/java/gabrieldev/com/supermercado_estoque/controllers/ProductController.java
@@ -20,9 +20,16 @@ import gabrieldev.com.supermercado_estoque.model.DTO.ProductDTO;
 import gabrieldev.com.supermercado_estoque.model.DTO.SimpleProductDTO;
 import gabrieldev.com.supermercado_estoque.services.ProductService;
 import gabrieldev.com.supermercado_estoque.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/product")
+@Tag(name = "Product", description = "Endpoints for Managing product")
 public class ProductController {
 
     @Autowired
@@ -31,6 +38,15 @@ public class ProductController {
     @GetMapping(value = "/{id}", 
     		produces = {
     				MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Find a Product", description = "Find a Product", tags = {"Product"}, 
+    responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    					@Content(schema = @Schema(implementation = ProductDTO.class))}),
+    		@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO productDTO = productService.findById(id);
         return ResponseEntity.ok(productDTO);
@@ -38,6 +54,16 @@ public class ProductController {
 
     @GetMapping(produces = {
     		MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Finds all Products", description = "Finds all Products", tags = {"Product"}, 
+    responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    					@Content(
+    							mediaType = "application/json",
+    							array = @ArraySchema(schema = @Schema(implementation = ProductDTO.class)))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<List<SimpleProductDTO>> findAll() {
         List<SimpleProductDTO> products = productService.findAll();
         return ResponseEntity.ok(products);
@@ -47,6 +73,13 @@ public class ProductController {
     		MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
     			produces = {
     					MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Adds new Product", description = "Adds new Product", tags = {"Product"}, 
+    responses = {
+    		@ApiResponse(description = "Created", responseCode = "200", content = {
+    					@Content(schema = @Schema(implementation = ProductDTO.class))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<ProductDTO> create(@RequestBody ProductDTO productDTO) {
         ProductDTO createdProduct = productService.create(productDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
@@ -56,12 +89,27 @@ public class ProductController {
     		MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
     			produces = {
     					MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Updates a Product", description = "Updates a Product", tags = {"Product"}, 
+    responses = {
+    		@ApiResponse(description = "Updated", responseCode = "200", content = {
+    					@Content(schema = @Schema(implementation = ProductDTO.class))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
         ProductDTO updatedProduct = productService.update(id, productDTO);
         return ResponseEntity.ok(updatedProduct);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes a Product", description = "Deletes a Product", tags = {"Product"}, 
+    responses = {
+    		@ApiResponse(description = "No content", responseCode = "204", content = {@Content}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         productService.delete(id);
         return ResponseEntity.noContent().build();
@@ -69,6 +117,16 @@ public class ProductController {
 
     @GetMapping(value = "/department/{departmentId}", produces = {
 			MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Get all Products from a Department", description = "Get all Products from a Department", tags = {"Product"}, 
+    responses = {
+    		@ApiResponse(description = "Success", responseCode = "200", content = {
+    					@Content(
+    							mediaType = "application/json",
+    							array = @ArraySchema(schema = @Schema(implementation = ProductDTO.class)))}),
+    		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    		@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
     public ResponseEntity<CollectionModel<EntityModel<ProductDTO>>> findByDepartment(@PathVariable Long departmentId) {
         CollectionModel<EntityModel<ProductDTO>> products = (CollectionModel<EntityModel<ProductDTO>>) productService.findByDepartment(departmentId);
         return ResponseEntity.ok(products);
